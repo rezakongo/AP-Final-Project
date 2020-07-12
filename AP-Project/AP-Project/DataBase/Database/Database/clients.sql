60b35cbeeb29ff8d560ff5f07573cf56b9c241f2
rezakongo <rezakongo8@gmail.com>
2020-07-12 13:00:10 +0430

    clients database initialized

---
+++ AP-Project/AP-Project/DataBase/Database/Database/clients.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[clients]
+(
+	[num] int NOT NULL,
+	firstname varchar(50) NOT NULL, 
+    [lastname] VARCHAR(50) NOT NULL, 
+    [phonenumber] INT NOT NULL, 
+    [nationalid] INT NOT NULL, 
+    [adress] VARCHAR(MAX) NOT NULL, 
+    [email] VARCHAR(MAX) NOT NULL, 
+    [password] VARCHAR(MAX) NOT NULL,
+)
