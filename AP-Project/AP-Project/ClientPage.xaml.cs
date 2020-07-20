bb9335ddf99a178b5e74dfc37810cb375425ed0e
rezakongo <rezakongo8@gmail.com>
2020-07-20 13:50:59 +0430

    all pages ok

--- AP-Project/AP-Project/ClientPage.xaml.cs
+++ AP-Project/AP-Project/ClientPage.xaml.cs
@@ -13,16 +13,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace AP_Project
 {
+
     /// <summary>
     /// Interaction logic for ClientPage.xaml
     /// </summary>
     public partial class ClientPage : Window
     {
         SqlConnection manager = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DtatBase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-
+        DataTable food = new DataTable();
         public ClientPage(string email)
         {
             InitializeComponent();
@@ -32,12 +34,91 @@ namespace AP_Project
             SqlDataAdapter adapter = new SqlDataAdapter(query, manager);
             adapter.Fill(table);
             hello.Text = "Hello "+table.Rows[0][0].ToString()+ "!!!";
-            DataTable food = new DataTable();
-            query = "select name,inventory from food";
+            
+            DataTable allfood = new DataTable();
+            query = "select * from food";
+            
             adapter = new SqlDataAdapter(query, manager);
             adapter.Fill(food);
             foodgrid.DataContext = food;
+            food.Columns.Add("Number");
+            for(int i = 0; i < food.Rows.Count; i++)
+            {
+                food.Rows[i][8] = "0";
+            }
+            
+           
+
             manager.Close();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable table = new DataTable();
+            table = ((DataView)foodgrid.ItemsSource).ToTable();
+            for (int j = table.Rows.Count - 1; j >= 0; j--)
+            {
+                if (table.Rows[j][2] == "")
+                {
+                    table.Rows[j][2] = "0";
+                }
+                if (!Regex.Match(table.Rows[j][2].ToString(), @"^[0-9]", RegexOptions.IgnoreCase).Success)
+                {
+                    table.Rows[j][2] = "0";
+                }
+                
+
+            }
+            foodgrid.DataContext = table;
+        }
+
+        private void filter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                manager.Open();
+                DataTable food = new DataTable();
+                DataTable allfood = new DataTable();
+                string query; query = "select * from food where type='" + ty.SelectedItem.ToString() + "'";
+                if (ty.SelectedItem.ToString() == "System.Windows.Controls.ComboBoxItem: All")
+                {
+                    query = "select * from food where day='" + cal.SelectedDate.ToString() + "'";
+                }
+                else
+                {
+                    query = "select * from food where type='" + ty.SelectedItem.ToString() + "' and day='" + cal.SelectedDate.ToString() + "'";
+                }
+                Console.WriteLine(ty.SelectedItem.ToString());
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, manager);
+                adapter.Fill(food);
+                foodgrid.DataContext = food;
+                food.Columns.Add("Number");
+                for (int i = 0; i < food.Rows.Count; i++)
+                {
+                    food.Rows[i][2] = "0";
+                }
+                manager.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void foodgrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                string r = foodgrid.SelectedIndex.ToString();
+                DataTable dt = new DataTable();
+                dt = ((DataView)foodgrid.ItemsSource).ToTable();
+                foodim.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(dt.Rows[int.Parse(r)][6]);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
--- AP-Project/AP-Project/ManagerPage.xaml.cs
+++ AP-Project/AP-Project/ManagerPage.xaml.cs
@@ -16,6 +16,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Security.Principal;
 using System.Text.RegularExpressions;
+using System.Security.Cryptography;
 
 namespace AP_Project
 {
@@ -53,6 +54,7 @@ namespace AP_Project
             if (System.Windows.Forms.DialogResult.OK.ToString() == "OK")
             {
                 image.Text = dialog.FileName.ToString();
+                im.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(image.Text.Trim());
             }
             
             
@@ -71,88 +73,142 @@ namespace AP_Project
 
         private void Changes_Click(object sender, RoutedEventArgs e)
         {
-            DataTable table = new DataTable();
-            table = ((DataView)foodtable.ItemsSource).ToTable();
-            for (int j = table.Rows.Count - 1; j >= 0; j--)
+            try
             {
-                if (table.Rows[j][5]=="" && table.Rows[j][1] == "" && table.Rows[j][2] == ""&& table.Rows[j][3] == "" && table.Rows[j][4] == "")
-                {
-                    table.Rows[j].Delete();
-                }
+                DataTable table = new DataTable();
+                table = ((DataView)foodtable.ItemsSource).ToTable();
+                string r = foodtable.SelectedIndex.ToString();
+                table.Rows[int.Parse(r)][7] = cal.SelectedDate.ToString();
+                table.Rows[int.Parse(r)][6] = it.Text.Trim();
+                table.Rows[int.Parse(r)][8] = t.SelectedItem.ToString();
                 
-            }
-
-            bool b=false;
-            bool a = false;
-
-            for (int j = 0; j < table.Rows.Count;j++)
-            {
-                b = false;
-                for(int k = 1; k < table.Columns.Count; k++)
+                for (int j = table.Rows.Count - 1; j >= 0; j--)
                 {
-                    if (table.Rows[j][k].ToString() == "")
+                    if (table.Rows[j][5] == "" && table.Rows[j][1] == "" && table.Rows[j][2] == "" && table.Rows[j][3] == "" && table.Rows[j][4] == "")
                     {
-                        b = true;
+                        table.Rows[j].Delete();
+                    }
+
+                }
+
+                bool b = false;
+                bool a = false;
+
+                for (int j = 0; j < table.Rows.Count; j++)
+                {
+                    b = false;
+                    for (int k = 1; k < 5; k++)
+                    {
+                        if (table.Rows[j][k].ToString() == "")
+                        {
+                            b = true;
+                            break;
+                        }
+                    }
+
+                    if (!Regex.Match(table.Rows[j][3].ToString(), @"^[0-9]", RegexOptions.IgnoreCase).Success)
+                    {
+                        a = true;
+                        MessageBox.Show("Block in row : " + (j + 1) + " and column : 4 should be a Number!!!");
                         break;
                     }
-                }
-                
-                if (!Regex.Match(table.Rows[j][3].ToString(), @"^[0-9]", RegexOptions.IgnoreCase).Success)
-                {
-                    a = true;
-                    MessageBox.Show("Block in row : " + (j+1) + " and column : 4 should be a Number!!!");
-                    break;
-                }
-                if (!Regex.Match(table.Rows[j][4].ToString(), @"^[0-9]", RegexOptions.IgnoreCase).Success)
-                {
-                    a = true;
-                    MessageBox.Show("Block in row : " + (j+1) + " and column : 5 should be a Number!!!");
-                    break;
-                }
-                if (!Regex.Match(table.Rows[j][5].ToString(), @"^[0-9]", RegexOptions.IgnoreCase).Success)
-                {
-                    a = true;
-                    MessageBox.Show("Block in row : " + (j+1) + " and column : 6 should be a Number!!!");
-                    break;
-                }
-                if (b)
-                {
-                    MessageBox.Show("Some Fields are NULL.Please try again");
-                    break;
-                }
+                    if (!Regex.Match(table.Rows[j][4].ToString(), @"^[0-9]", RegexOptions.IgnoreCase).Success)
+                    {
+                        a = true;
+                        MessageBox.Show("Block in row : " + (j + 1) + " and column : 5 should be a Number!!!");
+                        break;
+                    }
+                    if (!Regex.Match(table.Rows[j][5].ToString(), @"^[0-9]", RegexOptions.IgnoreCase).Success)
+                    {
+                        a = true;
+                        MessageBox.Show("Block in row : " + (j + 1) + " and column : 6 should be a Number!!!");
+                        break;
+                    }
+                    if (b)
+                    {
+                        MessageBox.Show("Some Fields are NULL.Please try again");
+                        break;
+                    }
 
 
 
-                
+                }
+                SqlConnection manager = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DtatBase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                manager.Open();
+                if ((!b) && (!a))
+                {
+
+                    SqlCommand del = new SqlCommand("delete from food", manager);
+                    del.ExecuteNonQuery();
+                    for (int i = 0; i < table.Rows.Count; i++)
+                    {
+                        string c = "insert into food values('" + (i + 1) + "','" + table.Rows[i][1] + "','" + table.Rows[i][2] + "','" + table.Rows[i][3] + "','" + table.Rows[i][4] + "','" + table.Rows[i][5] + "','" + table.Rows[i][6] + "','" + table.Rows[i][7] +"','"+table.Rows[i][8]+ "')";
+                        SqlCommand command = new SqlCommand(c, manager);
+                        command.ExecuteNonQuery();
+                    }
+
+                    r = table.Rows.Count.ToString();
+
+                }
+                SqlDataAdapter adapter1 = new SqlDataAdapter("select * from food", manager);
+                DataTable foods = new DataTable();
+                adapter1.Fill(foods);
+                foodtable.DataContext = foods;
+                manager.Close();
             }
-            SqlConnection manager = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DtatBase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            manager.Open();
-            if ((!b) && (!a))
+            catch
             {
-                
-                for (int i = 0; i < r; i++)
-                {
-                    string c = "update food set name='"+table.Rows[i][1].ToString()+"',caption='"+table.Rows[i][2]+"',initialPrice='"+table.Rows[i][3]+"',finalPrice='"+table.Rows[i][4]+"',inventory='"+table.Rows[i][5]+"' where num='"+(i+1)+"'";
-                    
-                    SqlCommand command = new SqlCommand(c, manager);
-                    command.ExecuteNonQuery();
-                    
-                }
-                for(int i = r; i < table.Rows.Count; i++)
-                {
-                    string c = "insert into food values('"+(i+1)+"','"+table.Rows[i][1]+"','" + table.Rows[i][2] + "','" + table.Rows[i][3] + "','" + table.Rows[i][4] + "','" + table.Rows[i][5] + "')";
-                    SqlCommand command = new SqlCommand(c,manager);
-                    command.ExecuteNonQuery();
-                }
-                
-                r = table.Rows.Count;
+
+            }
+        }
+
+        private void foodtable_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+
+        private void foodtable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SqlConnection manager = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DtatBase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            manager.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from food", manager);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            string r = foodtable.SelectedIndex.ToString();
+            try
+            {
+                it.Text = table.Rows[int.Parse(r)][6].ToString();
                 
             }
-            SqlDataAdapter adapter1 = new SqlDataAdapter("select * from food", manager);
-            DataTable foods = new DataTable();
-            adapter1.Fill(foods);
-            foodtable.DataContext = foods;
+            catch
+            {
+
+            }
             manager.Close();
+            try
+            {
+                fi.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(it.Text.Trim());
+            }
+            catch
+            {
+
+            }
+            
+
+
+        }
+
+        private void ci_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+            if (System.Windows.Forms.DialogResult.OK.ToString() == "OK")
+            {
+                it.Text = dialog.FileName.ToString();
+                fi.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(it.Text.Trim());
+            }
+
         }
     }
 }
